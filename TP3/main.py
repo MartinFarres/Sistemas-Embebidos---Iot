aa727ee55a2f868f9bcbfe65fdc57f1b026a63bc
MartinFarres <martinfarres@gmail.com>
2026-03-09 20:20:15 -0300

    Small fixes tp3

--- TP3/main.py
+++ TP3/main.py
@@ -8,7 +8,6 @@ import os
 import time
 import ntplib 
 
-
 app = FastAPI(
     title="FastAPI Template",
     version="0.1.0",
@@ -30,7 +29,6 @@ shareMem = {
     "serialConnected": False,
     "lastSerialError": "",
 }
-
 
 # Define el servidor NTP
 server = "pool.ntp.org"
@@ -69,14 +67,13 @@ def delete_EEPROM():
     try:
         s = get_serial()
         if s is not None:
-            # envia marcador 'L' 
+            # envia marcador 'B' (Corregido el marcador interno)
             ser.write(b'B\n')
             shareMem["events"] = []
             return {"status": "historial_eliminado"}
         return {"status": "error_puerto_cerrado"}
     except Exception as e:
         return {"status": "error", "error_msg": str(e)}
-
 
 @app.get("/events")
 def get_estado_sistema():
@@ -91,13 +88,12 @@ def startup_event():
     thread_serial_port = threading.Thread(target=listening_serial_port, name="Thread-SP", daemon=True)
     thread_serial_port.start()
 
-
 def get_serial():
     global ser
     if ser is None or not ser.is_open:
         try:
-            # ser = serial.Serial('/dev/ttyS1', 9600, timeout=1)
-            ser = serial.serial_for_url('rfc2217://localhost:4001', baudrate=9600, timeout=1)
+            ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+            # ser = serial.serial_for_url('rfc2217://localhost:4001', baudrate=9600, timeout=1)
             # ser = serial.serial_for_url('socket://127.0.0.1:4001', timeout=1)
             shareMem["serialConnected"] = True
             shareMem["lastSerialError"] = ""
@@ -111,25 +107,39 @@ def listening_serial_port():
     while True:
         try:
             s = get_serial()
-            line = s.readline().decode().strip()
+            
+            line = s.readline().decode('utf-8', errors='ignore').strip()
+
+            if line:
+                print("Arduino mandó:", line)
 
             if line and ',' in line:
                 # puerto serial envia: evento,tiempo\n
                 line_aux = line.split(",")            
-                event = line_aux[0]
-                timestamp_crudo = int(line_aux[1])
-                if event != "255" and timestamp_crudo > 0:
-                    res_time = time.ctime(int(line_aux[1]))
-                    shareMem["events"].append({
-                        "evento": event, 
-                        "hora": res_time
-                    })
+                event = line_aux[0].strip()
+                
+                try:
+                    timestamp_crudo = int(line_aux[1].strip())
+                    if event != "255" and timestamp_crudo > 0:
+                        res_time = time.ctime(timestamp_crudo)
+                        shareMem["events"].append({
+                            "evento": event, 
+                            "hora": res_time
+                        })
+                except Exception as e: 
+                    print(f"Dato corrupto ignorado: {line} - Motivo: {e}")
+                    pass
 
         except Exception as e:
             shareMem["serialConnected"] = False
             shareMem["lastSerialError"] = str(e)
+            
+            global ser
+            if ser is not None:
+                ser.close()
+                ser = None
+                
             time.sleep(0.5)
-
 
 # Conectar con Vue frontend
 FRONTEND_DIR = os.path.join(os.path.dirname(__file__), "frontend")
